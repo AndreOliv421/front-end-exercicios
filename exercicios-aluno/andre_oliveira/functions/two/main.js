97195dacbd20911ef11fcc407ca33d5f6a1653db
AndreOliv421 <andreoliv421@gmail.com>
2025-09-24 16:52:23 -0300

    up

---
+++ exercicios-aluno/andre_oliveira/functions/two/main.js
@@ -0,0 +1,6 @@
+function mostrarTipo(dado) {
+    console.log("Tipo da variável: ", typeof dado);
+}
+
+let entrada = prompt("Digite: ");
+mostrarTipo(entrada);
---
+++ exercicios-aluno/andre_oliveira/repeat-loops/do_while/two/main.js
@@ -0,0 +1,10 @@
+/* 3 */
+let entrada;
+
+do {
+      entrada = prompt("Digite 1 ou 0:");      
+} while (entrada !== "1" && entrada !== "0");
+alert("Você digitou: " + entrada);
+
+
+
